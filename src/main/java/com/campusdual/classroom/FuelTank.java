6cca6ae72d6223a53682df128e857f165be7eebf
Orlando Jose Garces Casal <ojgcasal@gmail.com>
2024-10-01 10:16:07 +0200

    Listo.

--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -2,13 +2,19 @@ package com.campusdual.classroom;
 
 public class FuelTank {
 
-  public int actualFuel = 10;
-
-  public void showDetails() {
-    System.out.println("La capacidad actual es de " + this.actualFuel + " litros.");
-  }
+  private int actualFuel = 10;
 
   public int getActualFuel() {
     return this.actualFuel;
+  }
+  public void setActualFuel(int fuel) {
+    if (fuel < 0) {
+      System.out.println("Error: No se puede establecer un valor negativo de combustible.");
+    } else {
+      this.actualFuel = fuel;
+    }
+  }
+  public void showDetails() {
+    System.out.println("La capacidad actual es de " + this.getActualFuel() + " litros.");
   }
 }
